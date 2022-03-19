12659ba5661007736e222645f4090d7081a474ea
Julio Betta <juliobetta@gmail.com>
2022-03-19 00:56:32 -0300

    upgrade solidity and web3 ecosystem

--- deploy.js
+++ deploy.js
@@ -1,13 +1,13 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
 
-const provider = new HDWalletProvider(
+const { abi, evm } = require('./compile');
+
+provider = new HDWalletProvider(
   'awesome coconut vote praise guess road black cube dial manual just venture',
-  // remember to change this to your own phrase!
   'https://rinkeby.infura.io/v3/67c5faeef93845f48a2e04c444679fee'
-  // remember to change this to your own endpoint!
 );
+
 const web3 = new Web3(provider);
 
 const deploy = async () => {
@@ -15,11 +15,12 @@ const deploy = async () => {
 
   console.log('Attempting to deploy from account', accounts[0]);
 
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
     .send({ gas: '1000000', from: accounts[0] });
 
   console.log('Contract deployed to', result.options.address);
-  provider.engine.stop(); // prevents a hanging deployment
+  provider.engine.stop();
 };
+
 deploy();
